83a4a16618695399a206e460e58378dd844431f5
Sai <saipranabpatra5233@gmail.com>
2025-03-11 00:03:25 +0530

    diff let, var, const & truthy, falsy

---
+++ 23.09.10/script.js
@@ -0,0 +1,87 @@
+// let, const & var => let const es6 (ecmascript)
+
+// refresh - 
+
+// father of js - Brendan Eich (developer - 10 days - Livescript - 1995 -> java - javascript)
+//  java or javascript => Netscape browser
+//  other browser internet explorer
+// Ecamscript organization - standardized - js
+// ES1, ES2, Es3, Es4, ES6 (2015)
+
+// var & function declaration -> both are hoisted
+// debugger
+
+// console.log(a)
+// var a = 10;
+// var b = 20;
+
+
+// console.log(a1)
+// console.log(a2)
+// let a1 = 100;
+// const a2 = 200;
+// v8 engine (chrome), SpiderMonkey engine (FireFox) -> JIT (Just In Time) compilation & Interpreted
+
+
+// var name = "JT"
+// var name = "Rashmi Sir"
+
+// const institueName = "JT"
+// const institueName = "Java Technocrat"
+
+// var course = "JS"
+// let course1 = "Java"
+
+
+// let x = 10;
+// x = 20;
+// console.log(x)
+
+// const y = 10;
+// y = 20;
+// console.log(y)
+
+
+// -------------------------truthy & falsy value-------------------------
+// falsy values 
+// false, 0, -0, "", null, undefined, Nan
+
+// if(true) { // false
+//     console.log("truthy value");
+// } else {
+//     console.log("falsy value")
+// }
+
+// if("jt") { // "", " "
+//     console.log("truthy value");
+// } else {
+//     console.log("falsy value")
+// }
+
+// if(-1) { // 0, -0
+//     console.log("truthy value");
+// } else {
+//     console.log("falsy value")
+// }
+
+// if(Infinity) { // null, undefined, NaN
+//     console.log("truthy value");
+// } else {
+//     console.log("falsy value")
+// }
+
+
+// alert("aloevera")
+
+// let fullname = prompt("Enter your fullname")
+// console.log(fullname);
+
+// let isNeedSleep = confirm("Do u want to sleep now ?")
+// console.log(isNeedSleep)
+
+
+
+
+console.log("Green")
+console.warn("Yellow")
+console.error("Red")
---
+++ 25.03.09/script.js
@@ -0,0 +1,27 @@
+// let a = 10;
+// a = 'js'
+
+// let, const, or var
+// 2015 - ES6 => let & const (recommended)
+
+// var a = 10; // global scope or function scope
+// hoisted
+// can be redeclared
+
+function show() {
+    // var a = 10;
+    // var a = 20;
+
+    // let a = 10;
+    // let a = 20;
+}
+
+v8 engine - JIT compilation and interpretation
+
+console.log('before declaration', b)
+var b = 200 // hoisted // undefined
+console.log('after declaration', b)
+
+console.log('before declaration', c)
+let c = 300; // not supported hoisting
+console.log('after declaration ', c)
