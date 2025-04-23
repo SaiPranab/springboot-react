bf6e9ad791ffb1bd2ccd359ab1f4e05c2c88bee4
Sai <saipranabpatra5233@gmail.com>
2025-04-24 00:07:57 +0530

    header start

--- 25.04.21 - quickhire/frontend/src/components/navbar/Header.jsx
+++ 25.04.21 - quickhire/frontend/src/components/navbar/Header.jsx
@@ -1,14 +1,18 @@
+import logo from "../../assets/e_logo.png"
+
 const Header = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg bg-primary-subtle">
             <div class="container-fluid">
+                <img src={logo} alt="" height="50" width="auto"/>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            
+                    
                 </div>
             </div>
             </nav>
